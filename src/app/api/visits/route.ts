812b2e6e38ada0a39f2c3adc837a9ee1345b2cc7
DiegoRojo99 <diego99rojo@gmail.com>
2025-05-12 21:25:35 +0200

    Get user visits

---
+++ src/app/api/visits/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+import { createClient } from '@/lib/supabase/server';
+import { PrismaClient } from '@prisma/client';
+import { MatchVisitWithDetails } from '@/types/includeDB';
+
+const prisma = new PrismaClient();
+
+export async function GET(req: Request) {
+  
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return new NextResponse('Unauthorized', { status: 401 });
+  }
+
+  const visits: MatchVisitWithDetails[] = await prisma.matchVisit.findMany({
+    where: { userId: user.id },
+    include: {
+      match: {
+        include: {
+          homeTeam: true,
+          awayTeam: true,
+          competition: true,
+          stadium: true,
+        },
+      },
+    },
+    orderBy: { createdAt: 'desc' },
+  });
+
+  return NextResponse.json(visits);
+}
