7dcdc067c0bc65b102b239103c5fa9806af5c692
DiegoRojo99 <diego99rojo@gmail.com>
2025-05-12 21:37:03 +0200

    Prisma generated client

--- src/app/api/visits/route.ts
+++ src/app/api/visits/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@/lib/supabase/server';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/generated/client';
 import { MatchVisitWithDetails } from '@/types/includeDB';
 
 const prisma = new PrismaClient();
