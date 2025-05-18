e588c462c467f3e9034cbaa13effb6c9707785b2
DiegoRojo99 <diego99rojo@gmail.com>
2025-05-18 12:00:42 +0200

    Photo form connected

--- src/app/visits/[id]/AddPhotoForm.tsx
+++ src/app/visits/[id]/AddPhotoForm.tsx
@@ -14,10 +14,14 @@ export default function AddPhotoForm({ visitId }: { visitId: string }) {
     formData.append('visitId', visitId);
 
     try {
-      await fetch('/api/photo/upload', {
+      const reponse = await fetch('/api/photo/upload', {
         method: 'POST',
         body: formData,
       });
+      if(!reponse.ok) {
+        const error = await reponse.json();
+        throw new Error(error.message);
+      }
       window.location.reload();
     } 
     catch (err) {
@@ -29,31 +33,20 @@ export default function AddPhotoForm({ visitId }: { visitId: string }) {
   };
 
   return (
-    <div className="mt-4 flex flex-col items-center">
+    <div className="m-4 flex flex-col items-center">
       <h2 className="text-lg font-semibold mb-2">Add a Photo</h2>
-      <div className="flex items-center justify-center w-[400px] mb-4">
-          <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-              <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-              </div>
-              <input 
-                id="dropzone-file" 
-                type="file" 
-                className="hidden"
-                onChange={(e) => setFile(e.target.files?.[0] || null)}
-                accept="image/*"
-                disabled={loading}
-              />
-          </label>
-      </div> 
+      <input
+        type='file'
+        accept='image/*'
+        onChange={(e) => setFile(e.target.files?.[0] || null)}
+        className="m-2 border border-gray-300 rounded p-2 flex flex-col items-center w-full"
+        disabled={loading}
+      />
       <button
         onClick={handleUpload}
         disabled={loading || !file}
-        className="ml-2 px-3 py-1 rounded bg-emerald-600 text-white hover:bg-emerald-700 disabled:opacity-50"
+        className="px-3 py-2 rounded bg-emerald-600 text-white hover:bg-emerald-700 
+          disabled:opacity-50 cursor-pointer transition duration-200 ease-in-out w-full"
       >
         {loading ? 'Uploading...' : 'Upload Photo'}
       </button>
