e2d16dfd5ca639e8b4bc9a1e7de5596e6d283749
DiegoRojo99 <diego99rojo@gmail.com>
2025-05-21 16:16:58 +0200

    Update empty cities with address

---
+++ src/scripts/address/teamCity.ts
@@ -0,0 +1,49 @@
+import { PrismaClient, Team } from '@prisma/generated/client';
+import { fallbackExtractCity } from './addressBreakdown';
+
+const prisma = new PrismaClient();
+
+async function updateTeamCities() {
+  // Fetch all teams with a null city
+  const teams: Team[] = await prisma.team.findMany({
+    where: { 
+      address: {
+        notIn: ['null null null', 'Unknown Address', 'Unknown Stadium', ''],
+        not: null,
+      },
+      city: null
+    },
+  });
+
+  for (const team of teams) {
+    // Check if the team has an address
+    if (!team.address) {
+      console.log(`No address found for team ${team.name}`);
+      continue;
+    }
+
+    // Use fallbackExtractCity to get the city
+    var city = fallbackExtractCity(team.address);
+    if (!city) {
+      console.log(`No city found for team ${team.name}`);
+      continue;
+    }
+
+    city = city.replaceAll('null', '');
+    // Update the team with the extracted city
+    await prisma.team.update({
+      where: { id: team.id },
+      data: { city },
+    });
+    console.log(`Updated team ${team.name} with city: ${city}`);
+    
+  }
+}
+
+updateTeamCities()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
